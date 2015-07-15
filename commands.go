92e5028f3dbbee208677778548d8ba05e9523118
Miquel Sabaté <msabate@suse.com>
2015-07-15 17:25:54 +0200

    Rough implementation of the list images
    This is a **really** simple implementation of the `images` command. It lacks
    of:
    
    - Proper checking of SUSE images.
    - A more fancy output.
    - Some checks.
    - Checking parents.
    
    This commit is just a proof of concept of the client library.

--- commands.go
+++ commands.go
@@ -14,9 +14,52 @@
 
 package main
 
-import "github.com/codegangsta/cli"
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/codegangsta/cli"
+	"github.com/samalba/dockerclient"
+)
+
+func stringInSlice(s string, strs []string) bool {
+	for _, str := range strs {
+		if str == s {
+			return true
+		}
+	}
+	return false
+}
+
+// TODO: right now this test is a joke. We should be checking the digest ID.
+func isSUSE(img *dockerclient.Image) bool {
+	allowed := []string{"13.1", "13.2", "harlequin",
+		"latest", "tumbleweed", "bottle"}
+
+	for _, repo := range img.RepoTags {
+		repoTag := strings.SplitN(repo, ":", 2)
+		if len(repoTag) == 2 && repoTag[0] == "opensuse" &&
+			stringInSlice(repoTag[1], allowed) {
+			return true
+		}
+	}
+	return false
+}
 
 func imagesCmd(ctx *cli.Context) {
+	client := getDockerClient()
+
+	imgs, err := client.ListImages(false)
+	if err != nil {
+		log.Printf("%v\n", err)
+	} else {
+		for _, img := range imgs {
+			if isSUSE(img) {
+				fmt.Printf("Img: %v\n", img)
+			}
+		}
+	}
 }
 func listUpdatesCmd(ctx *cli.Context) {
 }
